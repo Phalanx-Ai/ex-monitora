3bcf39b522ac3f2d3e0b17f9a6f56b89abc377f8
Marek Grac <marek@bedeep.eu>
2022-08-10 14:09:02 +0200

    [fix] Error messages to stdout instead of logging
    So they are visible in Keboola directly

--- src/component.py
+++ src/component.py
@@ -80,10 +80,10 @@ def read_feed(api_token, topic_monitor_id, last_id):
     )
 
     if response.status_code == 401:
-        logging.error("Authentication Failed")
+        print("Authentication Failed")
         sys.exit(1)
     elif response.status_code == 404:
-        logging.error("Invalid Feed ID")
+        print("Invalid Feed ID")
         sys.exit(1)
 
     while True:
